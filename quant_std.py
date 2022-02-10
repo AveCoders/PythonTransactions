f0e203be82b5dcb40c31543e8370e25c50292c66
V <52970004+AveCoders@users.noreply.github.com>
2022-02-10 18:24:27 +0000

    Add files via upload
    adds calculation of transactionAmount per category and good representation of the results

--- quant_std.py
+++ quant_std.py
@@ -1,10 +1,13 @@
 
+from collections import defaultdict
 from dataclasses import dataclass
-from itertools import groupby
+from itertools import chain, groupby
 import csv
 from csv import DictReader
+from re import A
 import statistics as mean
 import pprint
+from prometheus_client import Counter
 
 from pyrsistent import v 
 
@@ -68,17 +71,30 @@ def trans_type(trans):
     
     sorted_trans = sorted(trans, key=lambda x: (x.accountId, x.category)) # need to sort transactions or groupby won't work correctly (duplicates)
 
-    for k, g in groupby(sorted_trans, lambda x: (x.accountId, x.category)):
+    for k, g in groupby(sorted_trans, lambda x: (x.accountId, x.category)): #  # maybe not required
 
             
             cat_trans = [x.transactionAmount for x in g ]
             total_trans = sum(cat_trans)
             avg_val = total_trans / len(cat_trans)
-           
+            
+            
             print(f'AccountId: {k[0]}, transaction type {k[1]}, average value per transaction {avg_val:.2F}')
-       
+                
+d = defaultdict(float)
 
+def totals_per_cat(transactions):
     
+    for accountId, transactionAmount in chain.from_iterable(transactions):
+                 d[accountId] += transactionAmount
+    trans_res = list(d.values())
+    return trans_res
+    
+def unique_keys(transactions):
+    for accountId, transactionAmount in chain.from_iterable(transactions):
+                d[accountId] += transactionAmount
+    unique_keys = list(d.keys()) 
+    return unique_keys
 
 
 def rolling_window(trans, window_size, day):
@@ -89,6 +105,7 @@ def rolling_window(trans, window_size, day):
         return trans
     #for each day
 
+    # maybe not required
    # for i in range (day-1, day-window_size-1, -1): 
 
         for k, g in groupby(sorted_trans, lambda x: (x.accountId, x.category) ): # groups them by account Id and category
@@ -100,17 +117,34 @@ def rolling_window(trans, window_size, day):
             
             print(f'Day: {i} Account ID: {k[0]} Category/Type: {k[1]} Maximum: {day_amount} Average: {avg_val:.2F} Total: {total_trans:.2F}')
         
-    for i in range (day-1, day-window_size-1, -1): 
+    for i in range(day-window_size, day): 
         
-        cat_trans = ([x.transactionAmount for x in sorted_trans if x.transactionDay == i])
-        max_trans = max(cat_trans)
-        sum_days = sum(cat_trans)
-        acc_trans = ()
-        print(f'Day {i} {sum_days}')
-        #print(f'Day{i}, Account ID: {sorted_trans.accountId}, Category: {day.category}, Max: {max_trans} brought {cat_trans}')
+        total_trans = []
 
+        trans_AA = []
+        trans_CC = []
+        trans_FF = []
+
+        total_trans.append([(x.accountId, x.transactionAmount) for x in sorted_trans])
+
+        #proba = ([(x.accountId, x.transactionAmount) for x in sorted_trans])
+        
+        trans_AA.append([(x.accountId, x.transactionAmount) for x in sorted_trans  if x.category == "AA"])
+        trans_CC.append([(x.accountId, x.transactionAmount) for x in sorted_trans  if x.category == "CC"])
+        trans_FF.append([(x.accountId, x.transactionAmount) for x in sorted_trans  if x.category == "FF"])
+
+        
+
+        #TODO: return average transactionAmount per account for the last 5 days
+
+        #TODO: return maximum transactionAmount per account for the last 5 days
+        
+        for el in range(0, len(trans_AA)):
+                print(f'''Day {i}  Account ID    {unique_keys(trans_AA)[el]}    Maximum  "Placeholder"   Average   "Placeholder"   AA total value {totals_per_cat(trans_AA)[el]} CC total value {totals_per_cat(trans_CC)[el]} FF total value {totals_per_cat(trans_FF)[el]}  ''')
+                el +=1    
+      
 #daytrade_ops(trans)
 
-# trans_type(trans)
+#trans_type(trans)
 rolling_window(trans, 5, 10)
 
